50f31e87ae40591ca05767d77a40629e11daf17a
giovannanepomuceno-prog <giovanna.nepomuceno@escola.pr.gov.br>
2026-08-05 09:28:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-const botoes = document.querySelectorAll("button");
 
-botoes.forEach(function (botao) {
-    let curtiu = false;
-    botao.addEventListener("click", function() {
-        let texto = botao.querySelector("span");
-        if (curtiu === false) {
-            texto.textContent++;
-            curtiu = true;
-            botao.style.opacity = "0.7";
-        } else {
-            texto.textContent--;
-            curtiu = false;
-            botao.style.opacity = "1";
-        }
-    });
-});
